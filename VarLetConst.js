7324c3b2ea5691c95b70b6ff55dda7cb8a3fc6a2
Mason Bybee <bybeemason@gmail.com>
2023-05-21 00:28:58 -0500

    added VarLetConst exercise and solutions

---
+++ VarLetConst.js
@@ -0,0 +1,26 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+
+/*
+1. What is the difference between var and let?
+
+    Var has a function scope and can be redefined and can be accessed before it is declared while let has a block scope and its value can only be updated let can also only be accessed after its declaration
+
+2. What is the difference between var and const?
+
+    Var can be updated and redeclared, has a function scope, and can be accessed before it is declared.
+
+    Const primitive types cannot be updated, has block scope, and can only be accessed after its declared.
+
+3. What is the difference between let and const?
+
+    let can be updated no matter the type
+
+    const only non primative types can be updated
+
+4. What is hoisting?
+
+    Hoisting is when a var is accessed before its declared in the code. This returns undefined.
+*/
